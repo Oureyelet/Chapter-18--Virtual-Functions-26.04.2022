9813f1b436cbda1c2892b9155fa9bf699bd2c7da
Mateusz <oureyelet@gmail.com>
2022-05-03 18:41:23 +0100

    18.6 — The virtual table (vsCode)

--- 18.6 — The virtual table (vsCode)/main.cpp
+++ 18.6 — The virtual table (vsCode)/main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 
+class Base
+{
+public:
+    virtual void function1() { std::cout << "I'm Base function 1" << '\n'; }
+    virtual void function2() { std::cout << "I'm Base function 2" << '\n'; }
+};
+
+class D1: public Base
+{
+public:
+    virtual void function1() { std::cout << "I'm D1 function 1" << '\n'; }
+};
+
+class D2: public Base
+{
+public:
+    virtual void function2() { std::cout << "I'm D2 function 2" << '\n'; }
+};
+
 int main()
 {
-    
+    D1 d1; //d1 is a D1 object, d1 has its *__vptr set to the D1 virtual table.
+    Base* dPtr{ &d1 };
+    dPtr->function1();
+
+    Base base;
+    Base* pBase{ &base };
+    pBase->function1();
+
 
     return 0;
 }
