09707276bebc236cfa80e21594100aa222c1e31f
Mateusz <oureyelet@gmail.com>
2022-05-04 20:07:38 +0100

    18.9 — Object slicing (vsCode)

---
+++ 18.9 — Object slicing (vsCode)/main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+
+class Base
+{
+protected:
+    int m_value{};
+
+public:
+    Base(int value)
+        : m_value{ value }
+    {
+    }
+
+    virtual const char* getName() const { return "Base"; }
+    int getValue() const { return m_value; }
+};
+
+
+//FUNCTION PROTOTYPE:
+void printName_by_value(const Base base);
+void printName_by_references(const Base& base);
+
+class Derived: public Base
+{
+private:
+    double m_value{};
+
+public:
+    Derived(int value)
+        : Base{ value }
+    {
+    }
+
+    virtual const char* getName() const { return "Derived"; }
+    double getValue() const { return m_value; }
+
+    friend std::ostream& operator<<(std::ostream& out, const Base& pcos)
+    {
+        out << pcos.getValue() << '\n';
+        return out;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World my name is Sophie <3\n";
+
+    Derived derived{ 5 };
+    std::cout << "derived is a " << derived.getName() << " and has value " << derived.getValue() << '\n';
+
+    Base& ref{ derived };
+    std::cout << "ref is a " << ref.getName() << " and has value " << ref.getValue() << '\n';
+
+    Base* ptr{ &derived };
+    std::cout << "ptr is a " << ptr->getName() << " and has value " << ptr->getValue() << '\n';
+
+    Base base{ derived };
+    std::cout << "base is a " << base.getName() << " and has value " << base.getValue() << '\n';
+
+    //Slicing and functions:
+    printName_by_value(base);
+    printName_by_value(*ptr);
+    printName_by_value(derived);
+
+    std::cout << "Now with references... no copie ! :D" << '\n';
+
+    printName_by_references(base);
+    printName_by_references(*ptr);
+    printName_by_references(derived);
+
+    //Slicing vectors:
+
+    std::string array[3]{ ref.getName(), ptr->getName(), base.getName() };
+    for(int i{ 0 }; i < std::size(array); ++i)
+    {
+        std::cout << array[i] << ' ';
+    }
+    std::cout << std::endl;
+
+    Base array_test[3]{ ref.getValue(), ptr->getValue(), base.getValue() };
+    for(int i{ 0 }; i < std::size(array_test); ++i)
+    {
+        std::cout << array_test->getValue() << ' ';
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
+
+void printName_by_value(const Base base)// note: base passed by value, not reference
+{
+    std::cout << "I'm a " << base.getName() << '\n';
+}
+
+void printName_by_references(const Base& base)// note: base now passed by reference
+{
+    std::cout << "I'm a " << base.getName() << '\n';
+}
