6f381a3351bc3a27bf0a3f7ae502357cffbab57a
Mateusz <oureyelet@gmail.com>
2022-05-07 21:02:22 +0100

    -

--- 18.10 — Dynamic casting (vsCode)/main.cpp
+++ 18.10 — Dynamic casting (vsCode)/main.cpp
@@ -66,7 +66,7 @@ int main()
 
     Derived* d{ dynamic_cast<Derived*>(b) }; // use dynamic cast to convert Base pointer into Derived pointer
     
-    //dynamic_cast failure:
+    //dynamic_cast failure: :)
 
     if(d)// make sure d is non-null:  So simple check !
         std::cout << "The name of the Derived is: " << d->getName() << '\n';
@@ -88,7 +88,7 @@ int main()
     //Downcasting vs virtual functions:
 
     //A warning about dynamic_cast and RTTI:
-    
+
 
 
     
